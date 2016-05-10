a6932a654896e15f22ac6112a53dc5e9d91332e4
keenanamigo <keenan.johnson@rideamigos.com>
2016-05-10 20:44:24 +0000

    Working as expected.

--- free-code-camp/basic-algo-scripting/confirmEnding.js
+++ free-code-camp/basic-algo-scripting/confirmEnding.js
@@ -1,6 +1,13 @@
-function end(str, target) {
-  var check = str.substr(-1) === target;
-  return check;
+function confirmEnding(str, target) {
+  var ends = str.endsWith(target);
+  if (ends) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
 }
 
-end("Bastian", "n");
+confirmEnding("Bastian", "no");
+
+/*The ends variable checks to see if any given string ends with the string or char
+defined in the target*/
