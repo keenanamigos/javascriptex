756c25623a0e385cd6a2db76a3c37be43aa1720e
keenanamigo <keenan.johnson@rideamigos.com>
2016-06-01 16:10:54 +0000

    Added trim() to remove any unwanted whitespace

--- free-code-camp/truncateAString.js
+++ free-code-camp/truncateAString.js
@@ -2,9 +2,9 @@ function truncateString(str, num) {
   if (str.length <= num) {
      return str; //return entire string unaltered
   } else if (num > 3) {
-    return str.slice(0,num - 3) + "..."; //discard the last three characters because the elipses count towards num
+    return str.slice(0,num - 3).trim() + "..."; //discard the last three characters because the elipses count towards num
   } else if (num < 3) {
-    return str.slice(0, num) + "...";
+    return str.slice(0, num).trim() + "..."; //added .trim() to remove any unwanted whitespace
   }
 }
 
