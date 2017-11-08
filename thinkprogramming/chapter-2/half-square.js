f11108bcf3fcb8475465c180f4202639b15f2f6f
Keenan Johnson <keenan.johnson1@outlook.com>
2017-11-08 13:05:17 -0500

    Finished half-square problem

---
+++ thinkprogramming/chapter-2/half-square.js
@@ -0,0 +1,17 @@
+/* 
+    ** Using JS instead of C++ which changes the constraints
+    Write a program that produces a pattern of hash symbols shaped like half of a perfect 5 x 5 square (or a right triangle):
+    #####
+    ####
+    ###
+    ##
+    #
+*/
+
+const triangleHeight = 5; // 5 x 5 square 
+let hashSymbol = "#".repeat(triangleHeight);
+
+for (i = 0; i < triangleHeight; i++) {
+    console.log(hashSymbol);
+    hashSymbol = hashSymbol.substring(0, hashSymbol.length - 1);
+}
