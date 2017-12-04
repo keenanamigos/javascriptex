ebc7501a4a432f89229911c0f70ecf4ed9a47f43
Keenan Johnson <keenan.johnson1@outlook.com>
2017-12-03 22:59:09 -0600

    Require expect and Year in test file, use Chai assertions

---
+++ exercism/test/leap_test.js
@@ -0,0 +1,25 @@
+/* global describe it */
+const expect = require("chai").expect;
+const Year = require("../leap-year/leapYear").Year;
+
+describe("A leap year", () => {
+	it("year not divisible by 4: common year", () => {
+		const year = new Year(2015);
+		expect(year.isLeap()).toBeFalsy();
+	});
+
+	it("year divisible by 4, not divisible by 100: leap year", () => {
+		const year = new Year(2016);
+		expect(year.isLeap()).toBeTruthy();
+	});
+
+	it("year divisible by 100, not divisible by 400: common year", () => {
+		const year = new Year(2100);
+		expect(year.isLeap()).toBeFalsy();
+	});
+
+	it("year divisible by 400: leap year", () => {
+		const year = new Year(2000);
+		expect(year.isLeap()).toBeTruthy();
+	});
+});
