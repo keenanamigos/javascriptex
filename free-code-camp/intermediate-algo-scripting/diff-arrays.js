22b25aa7a86e3bd6df313ea9986ff7dfffdebf3c
Keenan Johnson <keenan.johnson1@outlook.com>
2018-01-11 14:29:53 -0600

    Finish Diff Two Arrays challenge

---
+++ free-code-camp/intermediate-algo-scripting/diff-arrays.js
@@ -0,0 +1,35 @@
+/**
+ * Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. 
+ * In other words, return the symmetric difference of the two arrays.
+ */
+
+function diffArray(arr1, arr2) {
+    const newArr = removeDuplicates(arr1.concat(arr2));
+    // Same, same; but different.
+    return newArr;
+  }
+  
+  function removeDuplicates(array) {
+    const uniqueValues = [];
+    
+    for (let i = 0; i < array.length; i++) {
+      if (array.indexOf(array[i]) === array.lastIndexOf(array[i])) {
+        uniqueValues.push(array[i]);
+      }
+    }
+    
+    return uniqueValues;
+  }
+  
+  diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+/** Test Cases */
+
+// diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]) should return an array.
+// ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["pink wool"].
+// ["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["diorite", "pink wool"].
+// ["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"] should return [].
+// [1, 2, 3, 5], [1, 2, 3, 4, 5] should return [4].
+// [1, "calf", 3, "piglet"], [1, "calf", 3, 4] should return ["piglet", 4].
+// [], ["snuffleupagus", "cookie monster", "elmo"] should return ["snuffleupagus", "cookie monster", "elmo"].
+// [1, "calf", 3, "piglet"], [7, "filly"] should return [1, "calf", 3, "piglet", 7, "filly"].
