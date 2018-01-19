9e83a4087e831c37b1c2e28652adff06fab1d198
Keenan Johnson <keenan.johnson1@outlook.com>
2018-01-19 13:57:37 -0500

    Initial correct sollution to Streamroller challenge

---
+++ free-code-camp/advanced-algo-scripting/streamroller.js
@@ -0,0 +1,22 @@
+function steamrollArray(arr) { 
+    // Solution
+    if (!Array.isArray(arr)) {
+      throw new Error(`${arr} is not an Array.`);
+    } 
+  
+    for (let i = 0; i < arr.length; i++) {
+        arr = arr.reduce((acc, current) => acc.concat(current), []);
+    }
+    
+    return arr;
+  }
+  
+  steamrollArray([1, [2], [3, [[4]]]]);
+
+  /* Test Cases */
+  /**
+   * steamrollArray([[["a"]], [["b"]]]) should return ["a", "b"]
+   * steamrollArray([1, [2], [3, [[4]]]]) should return [1, 2, 3, 4]
+   * steamrollArray([1, [], [3, [[4]]]]) should return [1, 3, 4]
+   * steamrollArray([1, {}, [3, [[4]]]]) should return [1, {}, 3, 4]
+   */
