e60c11d7440c2dc023e46ca691b58310c12c2f5f
Keenan Johnson <keenan.johnson1@outlook.com>
2018-01-19 13:26:47 -0500

    Add FCC setup code and test cases

--- free-code-camp/advanced-algo-scripting/record-collection.js
+++ free-code-camp/advanced-algo-scripting/record-collection.js
@@ -1,3 +1,33 @@
+// Setup
+const collection = {
+    "2548": {
+      "album": "Slippery When Wet",
+      "artist": "Bon Jovi",
+      "tracks": [ 
+        "Let It Rock", 
+        "You Give Love a Bad Name" 
+      ]
+    },
+    "2468": {
+      "album": "1999",
+      "artist": "Prince",
+      "tracks": [ 
+        "1999", 
+        "Little Red Corvette" 
+      ]
+    },
+    "1245": {
+      "artist": "Robert Palmer",
+      "tracks": [ ]
+    },
+    "5439": {
+      "album": "ABBA Gold"
+    }
+};
+// Keep a copy of the collection for tests
+const collectionCopy = JSON.parse(JSON.stringify(collection));
+
+// Solution
 function updateRecords(id, prop, value) {
     // Assumes a globally accessible collection
     const record = collection[id];
@@ -34,3 +64,16 @@ function isTracksProperty(property) {
 function recordHasProperty(record, property) {
     return record.hasOwnProperty(property);
 }
+
+// Alter values below to test your code
+updateRecords(5439, "artist", "ABBA");
+
+/* Test Cases */
+/**
+ * After updateRecords(5439, "artist", "ABBA"), artist should be "ABBA"
+ * After updateRecords(5439, "tracks", "Take a Chance on Me"), tracks should have "Take a Chance on Me" as the last element.
+ * After updateRecords(2548, "artist", ""), artist should not be set
+ * After updateRecords(1245, "tracks", "Addicted to Love"), tracks should have "Addicted to Love" as the last element.
+ * After updateRecords(2468, "tracks", "Free"), tracks should have "1999" as the first element.
+ * After updateRecords(2548, "tracks", ""), tracks should not be set
+ */
